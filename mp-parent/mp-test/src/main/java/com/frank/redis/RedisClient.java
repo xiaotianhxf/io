26c93ac75ac08870eeb28a24f42e00cc35c55bfe
hexiaofan <hexiaofan@meizu.com>
2017-08-29 20:13:16 +0800

    1

--- mp-parent/mp-test/src/main/java/com/frank/redis/RedisClient.java
+++ mp-parent/mp-test/src/main/java/com/frank/redis/RedisClient.java
@@ -1,7 +1,7 @@
 package com.frank.redis;
 
 import io.netty.bootstrap.Bootstrap;
-import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -9,18 +9,32 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 
 public class RedisClient {
-	
-	public void start() {
-		EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
-		Bootstrap bootstrap = new Bootstrap();
-		bootstrap.group(eventLoopGroup).remoteAddress("localhost", 6379)
-			.channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
-				@Override
-				protected void initChannel(SocketChannel socketChannel) throws Exception {
-//					socketChannel.pipeline().addLast(arg0)
-				}
-				
-			});
-	}
-	
+
+    public void start() {
+        EventLoopGroup eventLoopGroup = new NioEventLoopGroup();
+        Bootstrap bootstrap = new Bootstrap();
+
+        try {
+            bootstrap.group(eventLoopGroup).remoteAddress("localhost", 6379)
+                    .channel(NioSocketChannel.class).handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel socketChannel) throws Exception {
+                    socketChannel.pipeline().addLast(new RedisClientHandler());
+                }
+
+            });
+            ChannelFuture channelFuture = bootstrap.connect().sync();
+            channelFuture.channel().close().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            eventLoopGroup.shutdownGracefully();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new RedisClient().start();
+//		Thread.currentThread().setDaemon(true);
+    }
 }
--- mp-parent/mp-test/src/main/java/com/frank/redis/RedisClientHandler.java
+++ mp-parent/mp-test/src/main/java/com/frank/redis/RedisClientHandler.java
@@ -1,5 +1,11 @@
 package com.frank.redis;
 
-public class RedisClientHandler {
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
 
+public class RedisClientHandler extends ChannelInboundHandlerAdapter {
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        super.channelRead(ctx, msg);
+    }
 }
